48c1381039e18592483a3b4af49dab40be55e269
Fiammamuscari <fiammamuscari@gmail.com>
2024-05-04 00:41:07 -0300

    feat: modeled client

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <>
       <section>
-        <div className="text-5xl">$Asia</div>
+        <div className="bg-slate-800 p-36">
+          <div className="text-5xl text-black bg-white border-[.07em] p-40 rounded-sm border-solid border-black">
+            $Asia
+          </div>
+        </div>
       </section>
     </>
   );
