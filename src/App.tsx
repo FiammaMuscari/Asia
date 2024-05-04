b52d61ef9640f527b2cae4c248a7ebea216385f2
Fiammamuscari <fiammamuscari@gmail.com>
2024-05-04 01:02:06 -0300

    feat: adding social media

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,39 @@ function App() {
             $Asia
             <section className="flex flex-wrap justify-center items-center">
               <div className="w-full md:w-auto md:flex-grow-0 md:flex-shrink-0">
-                <a href="#">
+                <a
+                  href="https://dexscreener.com/solana/cukxjebbfdzb3jwj36a6y9dqtn33hurgwxveqxaizy2r"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    className="w-20  h-auto mx-auto md:mx-0 bigger-image"
+                    className="w-20 md:w-32 lg:w-48 h-auto mx-auto md:mx-0 max-w-full bigger-image"
                     src="/public/dexscreener.webp"
                     alt=""
                   />
                 </a>
               </div>
               <div className="w-full md:w-auto md:flex-grow-0 md:flex-shrink-0">
-                <a href="#">
+                <a
+                  href="https://t.me/asiacto"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    className="w-20  h-auto mx-auto md:mx-0 circle-image"
+                    className="w-20 md:w-32 lg:w-48 h-auto mx-auto md:mx-0 max-w-full circle-image"
                     src="/public/teg.webp"
                     alt=""
                   />
                 </a>
               </div>
               <div className="w-full md:w-auto md:flex-grow-0 md:flex-shrink-0">
-                <a href="#">
+                <a
+                  href="https://twitter.com/ASIAONSOL"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    className="w-20  h-auto mx-auto md:mx-0 circle-image"
+                    className="w-20 md:w-32 lg:w-48 h-auto mx-auto md:mx-0 max-w-full circle-image"
                     src="/public/X_icon.svg"
                     alt=""
                   />
